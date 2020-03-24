df0a10fe2a7af65049b5259726cca75fdc8488de
Mini Bhati <mini.bhati@mheducation.com>
2020-03-24 17:06:42 +0530

    Get APIs

--- server/app.js
+++ server/app.js
@@ -1,7 +1,43 @@
-const express = require('express');
+const express = require('express'),
+      fs =  require('fs'),
+      path = require('path'),
+      port = 3000;
+
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+function getAllBooks(){
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(__dirname, './stubs/books.json'), 'utf-8' , (err, contents)=> {
+      if (err) reject(err); // we'll not consider error handling for now
+      let jsonData = JSON.parse(contents.replace(/\r?\n|\r/g, ''));
+      resolve(jsonData);
+    });
+  });
+}
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+function getBookByIsbn(isbn){
+  return new Promise((resolve, reject) => {
+    getAllBooks().then(contents => {
+      const books = contents.books;
+      const bookInfo = books.filter( (book)=> {
+        console.log(book.isbn, "  ", isbn);
+        return book.isbn === isbn;
+      });
+      resolve(bookInfo);
+    });
+  });
+}
+
+app.get('/books', (req, res) => {
+  getAllBooks()
+    .then(contents => res.json(contents));
+});
+
+app.get('/books/:isbn', (req, res) => {
+  const isbn = req.params.isbn;
+  getAllBooks().then(contents => {
+    getBookByIsbn(isbn).then((bookInfo) => res.json(bookInfo));
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
