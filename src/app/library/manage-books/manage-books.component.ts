86b85d1614242f9f512e6387630505f0a3df83d8
Mini Bhati <mini.bhati@mheducation.com>
2020-05-17 12:58:38 +0530

    UI changes

--- src/app/library/manage-books/manage-books.component.ts
+++ src/app/library/manage-books/manage-books.component.ts
@@ -3,6 +3,7 @@ import { BooksService } from 'src/app/shared/services/books-service/books.servic
 import { ManageBooksService } from '../lib-services/services/manage-books/manage-books.service';
 import { AuthService } from '../lib-services/services/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-books',
@@ -13,28 +14,57 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ManageBooksComponent implements OnInit {
 
   allBooks: any = [];
+  filteredBooksList = [];
   dummyImgUrl = 'https://dummyimage.com/600x400/cccccc/000000.jpg&text=No+Cover';
   inProgressCount: number;
   completedCount = 0;
   selectedItem: any = [];
   currentState = 'all';
   viewType = 'thumbnail';
+  form: FormGroup;
+  optionsList: any = [
+    { id: 'title', name: 'Title' },
+    { id: 'page', name: 'Pagecount' }
+  ];
 
   constructor(private booksService: BooksService,
               private manageBooksService: ManageBooksService,
               private authService: AuthService,
               private route: ActivatedRoute,
-              private router: Router) {
-              this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+              private router: Router,
+              private formBuilder: FormBuilder) {
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+      this.form = this.formBuilder.group({
+        options: ['']
+      });
   }
 
   ngOnInit() {
     this.currentState = this.route.snapshot.paramMap.get('state');
     const userId = this.authService.currentUserValue;
     if (this.currentState === 'all') {
-      this.booksService.getAllBooks().subscribe(data => this.allBooks = data);
+      this.booksService.getAllBooks().subscribe(data => {
+        this.allBooks = data;
+        this.filteredBooksList = [...this.allBooks];
+      });
     } else {
       this.manageBooksService.getBooksByStatus(userId.username, this.currentState).subscribe((data: any) => this.allBooks = data.response);
+    }
+  }
+
+  get options() {
+    return this.form.get('options');
+  }
+
+  sortBy(e) {
+    const sortByValue = e.target.value;
+    this.options.setValue(sortByValue , {
+      onlySelf: true
+    });
+    if (sortByValue === '') {
+      this.filteredBooksList = [...this.allBooks];
+    } else if (sortByValue === 'title') {
+      this.filteredBooksList.sort((a, b) => a.title.localeCompare(b.title) );
     }
   }
 
